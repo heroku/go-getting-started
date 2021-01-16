3f32742b19fc91edd24addc303407adfbd092847
Joojo Dontoh <joojodontoh@gmail.com>
2021-01-16 16:48:31 +0800

    new repairs

--- main.go
+++ main.go
@@ -2,29 +2,51 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
-	"os"
 
-	"github.com/gin-gonic/gin"
+	"github.com/Joojo7/restaurant-order-system/router"
+	"github.com/gorilla/mux"
 	_ "github.com/heroku/x/hmetrics/onload"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	fmt.Print(port)
+	// port := os.Getenv("PORT")
+	// fmt.Print(port)
 
-	if port == "" {
-		port = "8000"
+	// if port == "" {
+	// 	port = "8000"
+	// }
+
+	// router := gin.New()
+	// router.Use(gin.Logger())
+	// router.LoadHTMLGlob("templates/*.tmpl.html")
+	// router.Static("/static", "static")
+
+	// router.GET("/tut", func(c *gin.Context) {
+	// 	c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	// })
+
+	myEnv, err1 := godotenv.Read()
+	if err1 != nil {
+		log.Fatal(err1)
 	}
+	port := myEnv["PORT"]
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+	myRouter := mux.NewRouter().StrictSlash(true)
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
+	// ROuter files
+	router.Routes(myRouter)
+	router.FoodRoutes(myRouter)
+	router.OrderItemRoutes(myRouter)
+	router.TableRoutes(myRouter)
+	router.InvoiceRoutes(myRouter)
+	router.OrderRoutes(myRouter)
 
-	router.Run(":" + port)
+	fmt.Printf("listening on %v \n", port)
+	error1 := http.ListenAndServe(port, myRouter)
+	if error1 != nil {
+		panic(error1)
+	}
 }
