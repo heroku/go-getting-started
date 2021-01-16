22003ec90cb48b57dedff6e96aedc67e0a72ff86
Joojo Dontoh <joojodontoh@gmail.com>
2021-01-16 19:27:55 +0800

    new changes

--- server.go
+++ server.go
@@ -5,18 +5,22 @@ import (
 	"golang-fifa-world-cup-web-service/handlers"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/subosito/gotenv"
 )
 
 func main() {
+	gotenv.Load()
 	data.Init()
 	data.PrintUsage()
-	myEnv, err1 := godotenv.Read()
+	_, err1 := godotenv.Read()
 	if err1 != nil {
 		log.Fatal(err1)
 	}
-	port := myEnv["PORT"]
+
+	port := os.Getenv("PORT")
 
 	http.HandleFunc("/", handlers.RootHandler)
 	http.HandleFunc("/winners", handlers.WinnersHandler)
