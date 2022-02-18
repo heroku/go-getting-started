345147bea279047e9147064258c4a929f5f98bc8
stregelemene21 <stregelemene21@gmail.com>
2022-02-18 16:56:47 +0300

    Markdown smth

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ go 1.17
 require (
 	github.com/gin-gonic/gin v1.7.4
 	github.com/heroku/x v0.0.33
+	github.com/russross/blackfriday v1.6.0
 )
 
 require (
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
+	//"github.com/russross/blackfriday"
 )
 
 func main() {
